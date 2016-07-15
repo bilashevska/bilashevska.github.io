c6393cbceb1d38b35a4e15763316dfeb1861250f
bilashevska <bilashevska@gmail.com>
2016-07-16 02:31:37 +0300

    some gulp files are added, also header is made

---
+++ gulp_sass/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var notify = require("gulp-notify");
+
+gulp.task('default', function () {
+  return gulp.src('css/*.css')
+    .pipe(concatCss('bundle.css'))
+    .pipe(cleanCSS())
+    .pipe(rename('bundle.min.css'))
+    .pipe(gulp.dest('out/'));
+});
+
+gulp.task('watch', function (){
+    gulp.watch('css/*css',['default'])
+})
