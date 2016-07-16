658964adff693d0d1676b00641e41b2b57e4fa59
bilashevska <bilashevska@gmail.com>
2016-07-16 22:52:31 +0300

    with sass

--- gulp_sass/gulpfile.js
+++ gulp_sass/gulpfile.js
@@ -1,17 +1,47 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var notify = require("gulp-notify");
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concatCss = require('gulp-concat-css'),
+	cleanCSS = require('gulp-clean-css'),
+	rename = require("gulp-rename"),
+	notify = require("gulp-notify"),
+	autoprefixer = require('gulp-autoprefixer'),
+	livereload = require('gulp-livereload')
+	connect = require('gulp-connect');
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'gulp_sass',
+    livereload: true
+  });
+});
 
-gulp.task('default', function () {
+gulp.task('css', function () {
   return gulp.src('css/*.css')
-    .pipe(concatCss('bundle.css'))
+    .pipe(concatCss('styles.css'))
+    .pipe(sass())
     .pipe(cleanCSS())
-    .pipe(rename('bundle.min.css'))
-    .pipe(gulp.dest('out/'));
+    .pipe(rename('styles.min.css'))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(livereload())
+    .pipe(gulp.dest('css/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  gulp.src('index.html')
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function (){
-    gulp.watch('css/*.css',['default'])
+    gulp.watch('scss/*.css',['default'])
+    gulp.watch('*.html',['default'])
 })
+gulp.task('sass', function(){
+  return gulp.src('scss/*.scss')
+    .pipe(sass()) // Конвертируем Sass в CSS с помощью gulp-sass
+    .pipe(gulp.dest('css'))
+});
+gulp.task('default', ['connect','html','sass', 'css', 'watch']);
