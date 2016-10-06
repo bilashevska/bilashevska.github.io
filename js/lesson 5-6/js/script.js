660ee7e5871bf4aa1a0683bdaac16b56dea4087f
bilashevska <bilashevska@gmail.com>
2016-10-06 17:42:07 +0300

    3d button is added

--- js/lesson 5-6/js/script.js
+++ js/lesson 5-6/js/script.js
@@ -2,27 +2,22 @@
  * Created by user on 04.10.2016.
  */
 var hours = document.querySelector('.hours'),
-  minutes = document.querySelector('.minutes')
-  seconds = document.querySelector('.seconds'),
-  milliseconds = document.querySelector('.milliseconds'),
-  start = document.querySelector('.start'),
-  finish = document.querySelector('.finish');
+    minutes = document.querySelector('.minutes')
+    seconds = document.querySelector('.seconds'),
+    milliseconds = document.querySelector('.milliseconds'),
+    start = document.querySelector('.start'),
+    pause = document.querySelector('.pause'),
+    clear = document.querySelector('.clear');
 
-
-
-
-start.onclick  = function () {
-  var ms = 0,
+var ms = 0,
     s = 1,
     m = 1,
     h = 1;
 
-  start.innerHTML = 'Pause';
 
-  timerId  = setInterval(function run() {
-
-    milliseconds.innerHTML = ms;
-    ms=ms+4;
+function run() {
+  milliseconds.innerHTML = ms;
+    ms = ms + 4;
     if(ms == 1000){
       ms = 0;
       seconds.innerHTML = s++;
@@ -37,21 +32,25 @@ start.onclick  = function () {
         }
       }
     }
-
-      start.onclick = function () {
-        clearInterval(timerId);
-        start.onclick = function () {
-          setInterval(run, 4);
-        }
-      }
-
-  }, 4);
-
-
-
-
-
-  finish.onclick =  function () {
-    clearInterval(timerId);
   }
+
+start.onclick  = function () {
+  timerId  = setInterval(run, 4);
 }
+
+pause.onclick = function () {
+  clearInterval(timerId);
+}
+
+clear.onclick =  function () {
+  clearInterval(timerId);
+  ms = 0;
+  s = 1;
+  m = 1;
+  h = 1;
+  milliseconds.innerHTML = '0';
+  seconds.innerHTML = '0';
+  minutes.innerHTML = '0';
+  hours.innerHTML = '0';
+}
+
