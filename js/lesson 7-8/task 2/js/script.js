8dabc54ac9c833dcc0e3425bbb02d414751d1801
bilashevska <bilashevska@gmail.com>
2016-10-09 13:44:33 +0300

    already ready

--- js/lesson 7-8/task 2/js/script.js
+++ js/lesson 7-8/task 2/js/script.js
@@ -3,10 +3,27 @@
  */
 
 $( function() {
+  $('button').on('click', function (e) {
+
+    e.preventDefault();
+
+    for (i = 0; i< 3;i++){
+
+      $('.tooltip').eq(i).remove();
+      $('<div class="tooltip"></div>').insertAfter($('input').eq(i))
+      $('.tooltip').eq(i).text($('input').eq(i).attr('title'));
+
+    }
+
+
+  })
 
   $("input").on("focusin", function (e) {
 
     e.preventDefault();
+
+    $('.tooltip').hide();
+    $('.tooltip').remove();
 
     $('<div class="tooltip"></div>').insertAfter($(this));
     $('.tooltip').text($(this).attr('title'));
@@ -20,6 +37,7 @@ $( function() {
     $('.tooltip').hide();
     $('.tooltip').remove();
   })
+
 
 } );
 
