9a0a201cf75156059a30535aaa4f97373500efa3
bilashevska <bilashevska@gmail.com>
2016-10-21 01:19:54 +0300

    lesson 9-10 has started

---
+++ js/lesson 9-10/js/script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by user on 20.10.2016.
+ */
+$(function() {
+  $('.jquery').iCheck({
+    checkboxClass: 'icheckbox_square-aero',
+  });
+  $(".basic-single").select2();
+  $('.jcarousel').jcarousel();
+
+  $('.jcarousel-control-prev')
+    .on('jcarouselcontrol:active', function() {
+      $(this).removeClass('inactive');
+    })
+    .on('jcarouselcontrol:inactive', function() {
+      $(this).addClass('inactive');
+    })
+    .jcarouselControl({
+      target: '-=1'
+    });
+
+  $('.jcarousel-control-next')
+    .on('jcarouselcontrol:active', function() {
+      $(this).removeClass('inactive');
+    })
+    .on('jcarouselcontrol:inactive', function() {
+      $(this).addClass('inactive');
+    })
+    .jcarouselControl({
+      target: '+=1'
+    });
+
+  $('.jcarousel-pagination')
+    .on('jcarouselpagination:active', 'a', function() {
+      $(this).addClass('active');
+    })
+    .on('jcarouselpagination:inactive', 'a', function() {
+      $(this).removeClass('active');
+    })
+    .jcarouselPagination();
+})
