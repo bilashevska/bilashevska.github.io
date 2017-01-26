1b704cb1cd3ac1741057940416cd7c1601776f17
bilashevska <bilashevska@gmail.com>
2017-01-26 21:22:08 +0200

    pixel perfect 960 768

--- markup/lesson 7-8/gulpfile.js
+++ markup/lesson 7-8/gulpfile.js
@@ -10,49 +10,55 @@ var imageMin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 
 gulp.task('sass', function() {
-    return sass('src/components/main.scss', { sourcemap: true, style: 'compact' })
-        .on('error', sass.logError)
-        .pipe(sourceMaps.init({loadMaps: true}))
-        .pipe(autoPrefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(rename('app.css'))
-        .pipe(cleanCSS())
-        .pipe(sourceMaps.write())
-        .pipe(gulp.dest('dist/css'));
+  return sass('src/components/main.scss', { sourcemap: true, style: 'compact' })
+    .on('error', sass.logError)
+    .pipe(sourceMaps.init({loadMaps: true}))
+    .pipe(autoPrefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(rename('app.css'))
+    .pipe(cleanCSS())
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('pages', function(){
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'))
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('fonts', function(){
-    return gulp.src('src/theme/fonts/*')
-        .pipe(gulp.dest('dist/css/fonts'))
+  return gulp.src('src/theme/fonts/*')
+    .pipe(gulp.dest('dist/css/fonts'))
 });
 
 gulp.task('images', function(){
-    return gulp.src('src/theme/images/*/*')
-        .pipe(gulp.dest('dist/images/'))
+  return gulp.src('src/theme/images/*/*')
+    .pipe(gulp.dest('dist/images/'))
+});
+// js-task
+gulp.task('js', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js/'))
 });
 
 gulp.task('webserver', function() {
-    gulp.src('dist')
-        .pipe(server({
-            livereload: {
-                enable: true,
-                filter: function(filePath, cb) {
-                    cb( !(/.DS_Store/.test(filePath)) );
-                }
-            },
-            directoryListing: false,
-            open: true,
-            log: 'info',
-            defaultFile: 'index.html'
-        }));
+  gulp.src('dist')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        filter: function(filePath, cb) {
+          cb( !(/.DS_Store/.test(filePath)) );
+        }
+      },
+      directoryListing: false,
+      open: true,
+      log: 'info',
+      defaultFile: 'index.html'
+    }));
 });
 
 gulp.task('default', function() {
-    gulp.start('pages', 'sass', 'images', 'fonts', 'webserver');
-    gulp.watch('src/*.html', ['pages']);
-    gulp.watch('src/components/**/*.scss', ['sass']);
+  gulp.start('pages', 'sass', 'images', 'fonts', 'webserver', 'js');
+  gulp.watch('src/*.html', ['pages']);
+  gulp.watch('src/components/**/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['js']);
 });
