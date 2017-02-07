1550d31330df648e8e9bdc114679c3e864ef2170
bilashevska <bilashevska@gmail.com>
2017-02-07 23:37:46 +0200

    something corrected

--- markup/lesson 7-8/dist/js/script.js
+++ markup/lesson 7-8/dist/js/script.js
@@ -1,6 +1,6 @@
 var sliderSet  = document.querySelectorAll('.slider__controller > div');
 
-for( var i = 0; i < sliderSet.length; i++) {
+for(var i = 0; i < sliderSet.length; i++) {
 	sliderSet[i].addEventListener('click', function() {
 		var active = document.querySelector('.slider__controller .active');
 		active.classList.remove('active');
@@ -10,7 +10,7 @@ for( var i = 0; i < sliderSet.length; i++) {
 
 var contentSet  = document.querySelectorAll('.info__controller > div');
 
-for( var i = 0; i < contentSet.length; i++) {
+for(var i = 0; i < contentSet.length; i++) {
 	contentSet[i].addEventListener('click', function() {
 		var active = document.querySelector('.info__controller .active');
 		active.classList.remove('active');
