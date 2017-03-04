867311f68133737d4c3516b44de00b23aaa72175
bilashevska <bilashevska@gmail.com>
2017-03-04 11:44:58 +0200

    add carousel instead slider

--- js/lesson 11-12/js/jquery.jcarousel.js
+++ js/lesson 11-12/js/jquery.jcarousel.js
@@ -1,43 +1,34 @@
-/*! jCarousel - v0.3.4 - 2015-09-23
- * http://sorgalla.com/jcarousel/
- * Copyright (c) 2006-2015 Jan Sorgalla; Licensed MIT */
 (function( $ ){
 
   $.fn.jcarousel = function() {
   
-     var leftUIEl = $('.jcarousel-control-prev');
+    var leftUIEl = $('.jcarousel-control-prev');
     var rightUIEl = $('.jcarousel-control-next');
     var elementsList = $('.jcarousel-list');
 
     var pixelsOffset = 300;
     var currentLeftValue = 0;
     var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount-1) * pixelsOffset);
+    var minimumOffset = - ((elementsCount - 1) * pixelsOffset);
     var maximumOffset = 0;
 
-    leftUIEl.addClass('non-active');
 
     leftUIEl.click(function() {
-      if (currentLeftValue != maximumOffset) {
-        rightUIEl.removeClass('non-active');
+      if (currentLeftValue !== maximumOffset) {
         currentLeftValue += 300;
-        elementsList.animate({ left : currentLeftValue + "px"}, 500);
+      } else {
+        currentLeftValue = minimumOffset;
       }
-      if(currentLeftValue == maximumOffset) {
-        leftUIEl.addClass('non-active');
-       }
+      elementsList.animate({ left : currentLeftValue + "px"}, 500);
     });
 
     rightUIEl.click(function() {
-      if (currentLeftValue != minimumOffset) {
-        leftUIEl.removeClass('non-active');
+      if (currentLeftValue !== minimumOffset) {
         currentLeftValue -= 300;
-        elementsList.animate({ left : currentLeftValue + "px"}, 500);
+      } else {
+        currentLeftValue = maximumOffset;
       }
-      console.log(currentLeftValue);
-      if(currentLeftValue == minimumOffset) {
-        rightUIEl.addClass('non-active');
-      }
+      elementsList.animate({ left : currentLeftValue + "px"}, 500);
     });
 
   };
