9c5b4a58ac89002767c98cbe74c22dcafec8aa85
bilashevska <bilashevska@gmail.com>
2017-03-09 21:50:22 +0200

    added dark background

--- js/lesson 13-14/js/script.js
+++ js/lesson 13-14/js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var key;
-var serObj;
+var serializeObj;
 var returnObj;
-var questions=[];
-var setQuestions = [
+var questions = [];
+var initialTemplate = [
   {
-    id:'quest1',
-    title: 'Вопрос №1',
+    id: 'quest1',
+    question: 'Вопрос №1',
     answer1: 'Ответ №1',
     answer2: 'Ответ №2',
     answer3: 'Ответ №3',
@@ -15,16 +15,16 @@ var setQuestions = [
 
   },
   {
-    id:'quest2',
-    title: 'Вопрос №2',
+    id: 'quest2',
+    question: 'Вопрос №2',
     answer1: 'Ответ №1',
     answer2: 'Ответ №2',
     answer3: 'Ответ №3',
     correct: '3'
   },
   {
-    id:'quest3',
-    title: 'Вопрос №3',
+    id: 'quest3',
+    question: 'Вопрос №3',
     answer1: 'Ответ №1',
     answer2: 'Ответ №2',
     answer3: 'Ответ №3',
@@ -32,10 +32,10 @@ var setQuestions = [
   }
 ];
 
-for (var i=0;i < setQuestions.length; i++){
-  key = 'question'+1;
-  serObj = JSON.stringify(setQuestions[i]);
-  localStorage.setItem(key, serObj);
+for (var i = 0; i < initialTemplate.length; i++) {
+  key = 'question' + 1;
+  serializeObj = JSON.stringify(initialTemplate[i]);
+  localStorage.setItem(key, serializeObj);
   returnObj = JSON.parse(localStorage.getItem(key));
   questions.push(returnObj);
 }
@@ -44,56 +44,60 @@ for (var i=0;i < setQuestions.length; i++){
 var template = _.template(document.getElementById('table-template').innerHTML);
 document.write(template(questions));
 
-var rad;
+var radio;
 var result;
 var messages = [];
-var current = questions[i];
+var current;
 var m = $('.msg');
 
-$(".form-button").click(function(){
-  rad = $('input[type = radio]') ;
-  for( var i = 0; i < rad.length; i++){
-    var temp = $(rad[i]);
-    if(temp.prop('checked') === true){
-      for( var j = 0; j < questions.length; j++){
-        if(questions[j].id === temp.attr('name')){
+$(".form-button").click(function () {
+  radio = $('input[type = radio]');
+  for (var i = 0; i < radio.length; i++) {
+    var temp = $(radio[i]);
+    if (temp.prop('checked') === true) {
+      for (var j = 0; j < questions.length; j++) {
+        if (questions[j].id === temp.attr('name')) {
           current = questions[j];
         }
       }
-      if(temp.val() === current.correct){
-        result = current.title + ': правильно.';
+      if (temp.val() === current.correct) {
+        result = current.question + ': правильно.';
       } else {
-        result = current.title + ': неправильно.'
+        result = current.question + ': неправильно.'
       }
       messages.push(result);
     }
   }
 
   m.append('<ul>');
-  if(messages.length == 0){
+  if (messages.length == 0) {
     messages[0] = 'Не дано ни одного ответа!'
     m.append('<p>' + messages[0] + '</p>');
   } else {
-    for(var i = 0; i < messages.length; i++){
+    for (var i = 0; i < messages.length; i++) {
       m.append('<li>' + messages[i] + '</li>')
     }
   }
   m.append('</ul>');
   event.preventDefault();
+  $('.overlay').fadeIn(400,
+    function(){
   $('.modal_form')
-  .css('display', 'block')
-  .animate({opacity: 1, top: '50%'}, 200);
+    .css('display', 'block')
+    .animate({opacity: 1, top: '50%'}, 200);
+});
 });
 
-$('.modal_close').click( function(){
+$('.modal_close, .overlay').click(function () {
   $('.modal_form')
     .animate({opacity: 0, top: '45%'}, 200,
-      function(){
+      function () {
         $(this).css('display', 'none');
+        $('.overlay').fadeOut(400);
       }
     );
 
-  messages.splice(0,messages.length);
+  messages.splice(0, messages.length);
   m.empty();
   document.getElementById('form').reset();
   $(".form-button").removeAttr('disabled', true);
